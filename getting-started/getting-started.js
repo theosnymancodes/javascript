8e84444282a6a165413d23a53633fd3e8f532085
Theo Snyman <theosnymancodes@gmail.com>
2024-08-09 15:21:03 +0200

    Explored some methods for serving JS code

---
+++ getting-started/getting-started.js
@@ -0,0 +1,6 @@
+console.log("JS code served from an external JS file.")
+
+/*
+    To run a JS file in the Node environment, use `node fileName.js` in
+    your terminal. Ensure your current directory (PWD) is where the JS file is located.
+*/
