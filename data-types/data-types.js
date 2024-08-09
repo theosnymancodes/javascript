250c4e3c93a8e4e27fa4cb2dbc6d2d36eb8c75c1
Theo Snyman <theosnymancodes@gmail.com>
2024-08-09 15:38:16 +0200

    Looked at primitive data types
    Not all primitives were covered yet.

---
+++ data-types/data-types.js
@@ -0,0 +1,13 @@
+/*
+    JS data types are either primitive (value) or reference (object) types.
+
+    - Primitive types are copied by value, so variables are independent.
+    - Reference types are copied by reference, so multiple variables can point to the same object.
+*/
+
+// Primitive types
+let stringType = "String"
+let numberType = 92 // Includes floating points
+let booleanType = false // Either true or false
+let undefinedType // Default for uninitialized variables, can also be assigned explicitly
+let nullType = null // Explicitly represents "no value"
